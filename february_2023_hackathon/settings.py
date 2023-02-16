6316e7041749cfb1b36e5a9cac2cdd16953b3f47
AdamBoley <adamboley92@gmail.com>
2023-02-16 19:42:08 +0000

    remove staticfiles finders

--- february_2023_hackathon/settings.py
+++ february_2023_hackathon/settings.py
@@ -137,12 +137,6 @@ STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
