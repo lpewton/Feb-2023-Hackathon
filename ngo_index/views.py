105705702c6c16b618e3c412b6a81451ed816768
AdamBoley <adamboley92@gmail.com>
2023-02-17 11:47:51 +0000

    add comments to index.html for front-end designers

--- ngo_index/views.py
+++ ngo_index/views.py
@@ -41,3 +41,65 @@ class NGO_Single(View):
             'NGO': ngo
         }
         return render(request, template_name, context)
+
+
+class NGO_Random(View):
+    """
+    View for displaying a random NGO page
+    """
+    def get(self, request, *args, **kwargs):
+        queryset = NGO.objects.order_by('?')
+        ngo = queryset.first()
+        template_name = 'random_ngo.html'
+        context = {
+            "NGO": ngo
+        }
+        return render(request, template_name, context)
+
+
+class SearchResults(generic.ListView):
+    def querystring(self):
+        """
+        querystring method
+        Required for retaining the same queryset across multiple -
+        - pagination pages
+        """
+        querystring = self.request.GET.copy()
+        querystring.pop(self.page_kwarg, None)
+        encoded_querystring = querystring.urlencode()
+        return encoded_querystring
+
+    def get_queryset(self):
+        """
+        get_queryset method
+        Constructs a queryset using Q methods
+        Returns an object_list for use within the template
+        """
+        query = self.request.GET.get('search')
+        object_list = NGO.objects.filter(
+            Q(name__icontains=query) |
+            Q(type__icontains=query) |
+            Q(founders__icontains=query) |
+            Q(director__icontains=query) |
+            Q(region__icontains=query) |
+            Q(location__icontains=query) |
+            Q(headquarters__icontains=query) |
+            Q(purpose__icontains=query)
+            ).filter(approved=True)
+        return object_list
+
+
+def http_404(request, exception):
+    """
+    Handles HTTP 404 Page Not Found errors
+    """
+    template_name = '404.html'
+    return render(request, template_name)
+
+
+def http_500(request):
+    """
+    Handles HTTP 500 Server Error errors
+    """
+    template_name = '500.html'
+    return render(request, template_name)
