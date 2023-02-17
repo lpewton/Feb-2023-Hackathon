6dfa55f13a52e5d5ce95ff89c0e4fb17e02268cb
AdamBoley <adamboley92@gmail.com>
2023-02-17 11:57:44 +0000

    add contact.html page

--- ngo_index/urls.py
+++ ngo_index/urls.py
@@ -4,5 +4,8 @@ from django.urls import path
 urlpatterns = [
     path('', views.LandingPage.as_view(), name='home'),
     path('all/', views.NGO_Directory.as_view(), name='ngo_directory'),
+    path('random/', views.NGO_Random.as_view(), name='random_review'),
+    path('search/', views.SearchResults.as_view(), name='search_results'),
+    path('contact/', views.Contact.as_view(), name='contact'),
     path('ngos/<int:pk>/', views.NGO_Single.as_view(), name='ngo_single'),
 ]
