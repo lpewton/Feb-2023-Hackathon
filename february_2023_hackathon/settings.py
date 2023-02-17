df839be314b7447f7940cb4c43a089ee2e84783d
AdamBoley <adamboley92@gmail.com>
2023-02-17 09:42:42 +0000

    add search-results and random templates

--- february_2023_hackathon/settings.py
+++ february_2023_hackathon/settings.py
@@ -41,10 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django_summernote',
     'cloudinary_storage',
+    'django.contrib.staticfiles',
     'cloudinary',
+    'django_summernote',
     'ngo_index',
 ]
 
@@ -137,7 +137,29 @@ STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SUMMERNOTE_THEME = 'bs5'
+
+SUMMERNOTE_CONFIG = {
+    'summernote': {
+        'width': '100%',
+        'height': '500',
+
+        'toolbar': [
+            ['style', ['bold', 'italic', 'underline', 'clear']],
+            ['font', ['strikethrough']],
+            ['para', ['ul', 'ol']],
+            ['insert', ['link']],
+            ['view', ['fullscreen', 'help']],
+        ],
+
+        'disableDragAndDrop': True
+    }
+}
